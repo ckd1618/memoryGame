f21610c9838a52697bd54e8c30fb8aee2969b9cb
Darakjian <cdarakjian@deloitte.com>
2019-12-02 14:23:26 -0800

    now working with NKJV

--- ui/public/src/app/home/home.component.ts
+++ ui/public/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { KJV } from '../../assets/bibles/KJV';
+import { NKJV } from '../../assets/bibles/NKJV';
 import { KJVarray, books66 } from '../../assets/bibles/KJVarray';
 
 @Component({
@@ -8,7 +9,8 @@ import { KJVarray, books66 } from '../../assets/bibles/KJVarray';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  versionChosen: string = 'KJV';
+  versionChosen: string = 'NKJV';
+  NKJVi = NKJV;
   KJVi = KJV;
   KJVarrayi = KJVarray;
   books66i = books66;
@@ -21,9 +23,15 @@ export class HomeComponent implements OnInit {
   verseLoc: string = "";
   showVerseVar: boolean = false;
 
+
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.NKJVi.books[64-1]['chapters'][3-1]['verses'][3-1].text);
+    // console.log(this.NKJVi.books[64-1].chapters[0].verses[1].text);
+    // console.log(this.KJVarrayi[64][1][1-1] === 14);
+    // console.log(this.NKJVi.books[64-1].chapters[1-1].verses[1-1].text);
+
   }
 
   consoleLog(){
@@ -63,21 +71,52 @@ export class HomeComponent implements OnInit {
   verseRenderer() {
     this.showVerseVar = false;
     if (this.userVerseObjCount === 0) {
+      this.verseToDisplay1 = "";
       this.verseToDisplay2 = "***All verses have been shown***";
+      this.verseToDisplay3 = "";
       this.verseLoc = "";
       return
     }
     var random1 = Math.floor(Math.random() * this.userVerseObjCount)+1;
     var three = this.userVerseObj[random1];
-    this.verseToDisplay1 = this.KJVi[this.KJVarrayi[three[0]][0]][three[1]][three[2]-1];
-    this.verseToDisplay2 = this.KJVi[this.KJVarrayi[three[0]][0]][three[1]][three[2]];
-    this.verseToDisplay3 = this.KJVi[this.KJVarrayi[three[0]][0]][three[1]][three[2]+1];
+    this.versionRouter(three, random1);
+
+  }
+
+  versionRouter(three, random1) {
+    if (this.versionChosen === 'NKJV') {
+      this.NKJVrenderer(three, random1);
+    } else {
+      this.KJVrenderer(three, random1);
+    }
+  }
+
+  KJVrenderer(three, random1){
+    // console.log(three);
+    var bookName = this.KJVarrayi[three[0]][0];
+    this.verseToDisplay1 = this.KJVi[bookName][three[1]][three[2]-1];
+    this.verseToDisplay2 = this.KJVi[bookName][three[1]][three[2]];
+    this.verseToDisplay3 = this.KJVi[bookName][three[1]][three[2]+1];
     this.verseLoc = `${this.KJVarrayi[three[0]][0]} ${three[1]}:${three[2]}`;
     //code below is if you wish to automatically remove the verse after it is displayed
     var doneVerseObjNum = this.userVerseObjCount--;
     this.userVerseObj[random1] = this.userVerseObj[doneVerseObjNum];
     delete this.userVerseObj[doneVerseObjNum];
   }
+
+  NKJVrenderer(three, random1) {
+    // console.log(three);
+    // console.log(three[2], this.KJVarrayi[three[0]][1][three[1]-1]);
+    this.verseToDisplay1 = three[2] === 1 ? "" : this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]-2].text;
+    this.verseToDisplay2 = this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]-1].text;
+    this.verseToDisplay3 = three[2] === this.KJVarrayi[three[0]][1][three[1]-1] ? "" : this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]].text;
+    this.verseLoc = `${this.KJVarrayi[three[0]][0]} ${three[1]}:${three[2]}`;
+    //code below is if you wish to automatically remove the verse after it is displayed
+    var doneVerseObjNum = this.userVerseObjCount--;
+    this.userVerseObj[random1] = this.userVerseObj[doneVerseObjNum];
+    delete this.userVerseObj[doneVerseObjNum];
+  }
+
   showVerse() {
     this.showVerseVar = !this.showVerseVar;
   }
