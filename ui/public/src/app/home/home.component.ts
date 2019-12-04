464c26d6ce98d02b7d8dab2c2277e43a86474917
Darakjian <cdarakjian@deloitte.com>
2019-12-04 02:05:02 -0800

    viewing the entire book with the selected verse bolded now is functional

--- ui/public/src/app/home/home.component.ts
+++ ui/public/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { KJV } from '../../assets/bibles/KJV';
 import { NKJV } from '../../assets/bibles/NKJV';
-import { KJVarray, books66 } from '../../assets/bibles/KJVarray';
+import { KJVarray, books66, bookNum } from '../../assets/bibles/KJVarray';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,12 @@ import { KJVarray, books66 } from '../../assets/bibles/KJVarray';
 })
 export class HomeComponent implements OnInit {
   versionChosen: string = 'NKJV';
+  fullText;
   NKJVi = NKJV;
   KJVi = KJV;
   KJVarrayi = KJVarray;
   books66i = books66;
+  bookNumi = bookNum;
   showSelector = true;
   userVerseObj ={};
   userVerseObjCount = 0;
@@ -21,17 +23,21 @@ export class HomeComponent implements OnInit {
   verseToDisplay2: string;
   verseToDisplay3: string;
   verseLoc: string = "";
+  bookLoc: string = "";
+  bookLocNum: number;
+  chapterLoc: string = "";
+  chapterLocNum: number;
+  verseOnlyLoc: string = "";
+  verseOnlyLocNum: number;
   showVerseVar: boolean = false;
   noVersesSelected: string = "";
+  showBookVar: boolean = false;
 
 
   constructor() { }
 
   ngOnInit() {
-    // console.log(this.NKJVi.books[64-1]['chapters'][3-1]['verses'][3-1].text);
-    // console.log(this.NKJVi.books[64-1].chapters[0].verses[1].text);
-    // console.log(this.KJVarrayi[64][1][1-1] === 14);
-    // console.log(this.NKJVi.books[64-1].chapters[1-1].verses[1-1].text);
+
 
   }
 
@@ -46,7 +52,6 @@ export class HomeComponent implements OnInit {
     this.books66i[i][2][j] = !this.books66i[i][2][j];
   }
   begin(){
-    
     this.userVerseObjPopulator();
   }
   userVerseObjPopulator() {
@@ -54,7 +59,6 @@ export class HomeComponent implements OnInit {
     this.userVerseObj = {};
     for (let i = 0; i < this.books66i.length; i++) {
       if (this.books66i[i][1]) {
-        // console.log(this.books66i[i][2].length);
         for (let j = 0; j < this.KJVarrayi[i][1].length; j++) {
           if(this.books66i[i][2][j]) {
             for (let k = 1; k <= this.KJVarrayi[i+1][1][j]; k++) {
@@ -73,8 +77,6 @@ export class HomeComponent implements OnInit {
       this.noVersesSelected = "Please make a selection in order to begin."
     }
     
-    // Math.floor(Math.random() * this.userVerseObjCount)+1
-    // this.KJVarrayi[three[0]][0]
   }
   verseRenderer() {
     this.showVerseVar = false;
@@ -106,6 +108,12 @@ export class HomeComponent implements OnInit {
     this.verseToDisplay2 = this.KJVi[bookName][three[1]][three[2]];
     this.verseToDisplay3 = this.KJVi[bookName][three[1]][three[2]+1];
     this.verseLoc = `${this.KJVarrayi[three[0]][0]} ${three[1]}:${three[2]}`;
+    this.bookLoc = this.KJVarrayi[three[0]][0];
+    this.chapterLoc = three[1];
+    this.verseOnlyLoc = three[2];
+    this.bookLocNum = parseInt(this.KJVarrayi[three[0]][0]);
+    this.chapterLocNum = parseInt(three[1]);
+    this.verseOnlyLocNum = parseInt(three[2]);
     //code below is if you wish to automatically remove the verse after it is displayed
     var doneVerseObjNum = this.userVerseObjCount--;
     this.userVerseObj[random1] = this.userVerseObj[doneVerseObjNum];
@@ -113,12 +121,18 @@ export class HomeComponent implements OnInit {
   }
 
   NKJVrenderer(three, random1) {
-    // console.log(three);
+    console.log(three);
     // console.log(three[2], this.KJVarrayi[three[0]][1][three[1]-1]);
     this.verseToDisplay1 = three[2] === 1 ? "" : this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]-2].text + " ";
     this.verseToDisplay2 = this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]-1].text + " ";
     this.verseToDisplay3 = three[2] === this.KJVarrayi[three[0]][1][three[1]-1] ? "" : this.NKJVi.books[three[0]-1].chapters[three[1]-1].verses[three[2]].text + " ";
     this.verseLoc = `${this.KJVarrayi[three[0]][0]} ${three[1]}:${three[2]}`;
+    this.bookLoc = this.KJVarrayi[three[0]][0];
+    this.chapterLoc = three[1];
+    this.verseOnlyLoc = three[2];
+    this.bookLocNum = parseInt(this.KJVarrayi[three[0]][0]);
+    this.chapterLocNum = parseInt(three[1]);
+    this.verseOnlyLocNum = parseInt(three[2]);
     //code below is if you wish to automatically remove the verse after it is displayed
     var doneVerseObjNum = this.userVerseObjCount--;
     this.userVerseObj[random1] = this.userVerseObj[doneVerseObjNum];
@@ -130,7 +144,7 @@ export class HomeComponent implements OnInit {
   }
 
   toggleChapters(i) {
-    var length = this.books66i[i][2].length;
+    var length = this.KJVarrayi[i+1][1].length;
     var arrOfTF = [];
 
     for (let idx = 0; idx < length; idx++) {
@@ -142,6 +156,28 @@ export class HomeComponent implements OnInit {
     }
     this.books66i[i][3] = !this.books66i[i][3];
     this.books66i[i][2] = arrOfTF;
+  }
+  showBook() {
+    if (this.showBookVar === true) {
+      this.fullText = [];
+      this.showBookVar = false;
+      return ;
+    }
+    this.showBookVar = true;
+    if (this.versionChosen === 'NKJV'){
+      this.fullText = [];
+      var NKJVchaptersNum: number = this.NKJVi.books[parseInt(this.bookNumi[this.bookLoc])-1].chapters.length;
+      for (let i = 0; i < NKJVchaptersNum; i++) {
+        var tempChapter = [];
+        var currentChapter = this.NKJVi.books[parseInt(this.bookNumi[this.bookLoc])-1].chapters[i].verses
+        for (let j = 0; j < currentChapter.length; j++) {
+          tempChapter.push(currentChapter[j].text);
+        }
+        this.fullText.push(tempChapter);
+      }
+    }else {
+
+    }
   }
   //esv to be added
 }
